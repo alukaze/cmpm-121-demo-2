5b567427a4a4db6e29543e5733ac93a340c421c7
Alan Lu <alu36@ucsc.edu>
2024-10-23 00:44:01 -0700

    Step 2 Done

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,40 @@ const title = document.createElement('h1');
 title.textContent = 'Gamin';
 app.appendChild(title);
 
-const canvas = document.createElement('canvas');
-app.appendChild(canvas);
+// Canvas
+const canvas = document.getElementById("display") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+// Buttons
+const clear = document.createElement('button');
+clear.textContent = "clear";
+app.appendChild(clear);
+
+// Drawing
+const cursor = {active: false, x:0, y:0};
+
+canvas.addEventListener("mousedown", (event) => {
+    cursor.active = true;
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+  });
+
+
+canvas.addEventListener("mousemove", (event) => {
+    if (cursor.active) {
+      ctx?.beginPath();
+      ctx?.moveTo(cursor.x, cursor.y);
+      ctx?.lineTo(event.offsetX, event.offsetY);
+      ctx?.stroke();
+      cursor.x = event.offsetX;
+      cursor.y = event.offsetY;
+    }
+  });
+
+  canvas.addEventListener("mouseup", (event) => {
+    cursor.active = false;
+  });
+
+  clear.addEventListener("click", () => {
+    ctx?.clearRect(0, 0, canvas.width, canvas.height);
+  });
