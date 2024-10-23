a2ba8b404b908376e25f4c3e7025fa21c9402793
Alan Lu <alu36@ucsc.edu>
2024-10-23 00:06:47 -0700

    Step 1 Done

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,10 @@ const APP_NAME = "Game 2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement('h1');
+title.textContent = 'Gamin';
+app.appendChild(title);
+
+const canvas = document.createElement('canvas');
+app.appendChild(canvas);
